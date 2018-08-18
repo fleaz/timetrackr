75bfd52f9738664c3de6fc14923e2ba4bb74ad1b
Felix Breidenstein <mail@felixbreidenstein.de>
2018-08-18 02:16:57 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,24 @@
+# timetrackr
+
+![screenshot of the tool](screenshot.png)
+
+A small tool to help you keep track of your working hours.
+This is especially helpfull if you switch customers regularry and need multiple stopwatches.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="container">
+  <h1 class="title">Timetrackr</h1>
+  <div class="columns" v-for="(t,id) in timer" :key="id">
+    <div class="column">
+      <input  v-model='t.title' size="80" :disabled="t.state === 'stopped' ? false:true">
+    </div>
+    <div class="column time">
+      {{ toHHMMSS(t.runTime) }}
+    </div>
+    <div class="column">
+      <button class="button is-success" v-on:click="start(id)" :disabled="t.state !== 'running' ? false : true"><b-icon icon="play"></b-icon></button>
+      <button class="button is-warning" v-on:click="pause(id)" :disabled="t.state === 'running' ? false: true"><b-icon icon="pause"></b-icon></button>
+      <button class="button is-danger" v-on:click="reset(id)" :disabled="t.state === 'paused' ? false: true"><b-icon icon="restart"></b-icon></button>
+    </div>
+  </div>
+  <button class="button is-primary" v-on:click="addTimer"><b-icon icon="plus"></b-icon></button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      timer: {
+        0: {
+          interval: null,
+          title: '',
+          runTime: 0,
+          state: 'stopped'
+        }
+      }
+    }
+  },
+  methods: {
+    pad: function (time) {
+      return (time < 10 ? '0' : '') + time
+    },
+    toHHMMSS: function (runtime) {
+      var hours = Math.floor(runtime / 3600)
+      var minutes = Math.floor((runtime - (hours * 3600)) / 60)
+      var seconds = runtime - (hours * 3600) - (minutes * 60)
+
+      return this.pad(hours) + ':' + this.pad(minutes) + ':' + this.pad(seconds)
+    },
+    start: function (id) {
+      this.timer[id].interval = setInterval(() => this.stopwatch(id), 1000)
+      this.timer[id].state = 'running'
+    },
+    pause: function (id) {
+      clearInterval(this.timer[id].interval)
+      this.timer[id].interval = null
+      this.timer[id].state = 'paused'
+    },
+    reset: function (id) {
+      this.timer[id].interval = null
+      this.timer[id].runTime = 0
+      this.timer[id].state = 'stopped'
+      this.timer[id].title = ''
+    },
+    stopwatch: function (id) {
+      this.timer[id].runTime += 1
+    },
+    addTimer: function (id) {
+      const l = Object.keys(this.timer).length
+      console.log('lol')
+      this.$set(this.timer, l, {
+        interval: null,
+        title: '',
+        runTime: 0,
+        state: 'stopped'
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.columns {
+  align-items: center;
+}
+
+.time {
+  text-align: center;
+  font-weight: bold;
+  font-size: 120%;
+  font-family: monospace;
+}
+</style>
